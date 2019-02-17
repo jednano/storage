e1524b2ea7cd158469465b7de19fe328dc794f27
Jed Mao <jedmao@outlook.com>
2019-02-17 08:53:21 -0600

    v1.0.0 - Initial implementation

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,23 @@
-export default function index() {
-	return 42
+export default class WebStorage implements Storage {
+	private storage: { [key: string]: string } = {}
+	public get length() {
+		return Object.keys(this.storage).length
+	}
+	public clear() {
+		this.storage = {}
+	}
+	public getItem(key: string) {
+		return this.storage[key]
+	}
+	public key(index: number) {
+		return Object.keys(this.storage)[index]
+	}
+	public removeItem(key: string) {
+		delete this.storage[key]
+	}
+	public setItem(key: string, value: string) {
+		this.storage[key] = value.toString()
+	}
 }
 
 // @ts-ignore
